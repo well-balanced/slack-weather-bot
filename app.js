79740606f12151d91b3e4c077e1fba2be0071e0b
cosmian <softkey95@gmail.com>
2019-11-20 02:06:31 +0900

    fixing bug

--- app.js
+++ app.js
@@ -61,6 +61,6 @@ const crawler = async() => {
 
 crawler()
 
-  // app.listen(3000, function () {
-  //   console.log('Example app listening on port 3000!');
-  // });
+  app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+  });
