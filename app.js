f666f01e873e8de1a5f3e99605ac73e997400627
cosmian <softkey95@gmail.com>
2019-11-20 00:09:12 +0900

    some notification message changed

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const axios = require('axios')
 const iconv = require('iconv-lite');
 
 // Crawling
-//image_url= 'https://cosmian.tistory.com/42'
 target_url = 'https://www.weather.go.kr/weather/forecast/timeseries.jsp'
 const crawler = async() => {
   const response = await axios.get(target_url,{
@@ -29,85 +28,37 @@ const crawler = async() => {
     const tomorrowAverTmp = (tomorrowLowTmp+tomorrowHighTmp)/2
     tomorrowLook = (tomorrowAverTmp) => {
       if (tomorrowAverTmp<4){
-        return '날씨가 춥네요. 패딩과 두꺼운 코트가 좋겠어요.\n목도리나 기모제품도 챙겨주는 센스!'
+        return '날씨가 춥네요. 패딩과 두꺼운 코트가 좋겠어요. 목도리나 기모제품도 챙겨주는 센스!'
       } else if (tomorrowAverTmp<8){
-        return '코트나 가죽자켓, 니트 정도가 좋겠네요.\n히트텍은 필수!'
+        return '코트나 가죽자켓, 니트 정도가 좋겠네요. 히트텍은 필수!'
       } else if (tomorrowAverTmp<11){
-        return '멋부리기 좋은 계절이에요^^*\n트렌치코트나 야상, 자켓은 어떨까요?'
+        return '멋부리기 좋은 계절이에요 *^^* 트렌치코트나 야상, 자켓은 어떨까요?'
       } else if (tomorrowAverTmp<16){
-        return '자켓이나 가디건은 어때요?\n없다구요? 그러니까 여자친구가 없다구요.'
+        return '자켓이나 가디건은 어때요? 없다구요? 그러니까 여자친구가 없다구요.'
       } else if (tomorrowAverTmp<20){
-        return '흰색 긴팔에 청바지는 사랑입니다.\n밤에는 추울 수 있으니 얇은 겉옷도!'
+        return '흰색 긴팔에 청바지는 사랑입니다. 밤에는 추울 수 있으니 얇은 겉옷도!'
       } else if (tomorrowAverTmp<24){
-        return '반팔이나 얇은 셔츠, 반바지나 면바지.\n어쨌든 가볍게 입고나와요~'
+        return '반팔이나 얇은 셔츠, 반바지나 면바지. 어쨌든 가볍게 입고나와요~'
       } else {
-        return '쪄죽을 날씨에요. 닥 민소매. \n선크림도 당연히 필수겠죠?'
+        return '쪄죽을 날씨에요. 닥 민소매. 선크림도 당연히 필수겠죠?'
       }};
       schedule.scheduleJob('00 23 * * *', ()=>{
-        //(async() =>{
-          //await 
           webhook.send({
             type: 'mrkdwn',
-            // accessory: {
-            //   type: 'image',
-            //   image_url: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FbkiKoB%2FbtqzQ8pBFMr%2F4PKp0rLrhS3qAcr1SX4TK1%2Fimg.jpg"
-            //},
             attachments:[{
               color: "#FFFFFF",
               fields:[
               {
                 title:`날씨봇 ON !!! 기온과 옷차림을 알려드리겠습니다.`,
-                value:`-----------------------------------------------------\n오늘 ${location}은 *최고 ${todayHighTmp}°, 최저 ${todayLowTmp}°* 였으며,\n
-                내일 ${location}은 *최고 ${tomorrowHighTmp}°, 최저 ${tomorrowLowTmp}°* 입니다.\n-----------------------------------------------------${tomorrowLook(tomorrowAverTmp)}`,
+                value:`-\n오늘 ${location}은 *최고 ${todayHighTmp}°, 최저 ${todayLowTmp}°* 였으며,\n내일 ${location}은 *최고 ${tomorrowHighTmp}°, 최저 ${tomorrowLowTmp}°* 입니다.\n\n${tomorrowLook(tomorrowAverTmp)}`,
                 short:false}
                   ]}],
-        
-            
             })
-          // })
         })
       }
     };
-    // const response = await axios.get(image_url,{
-    //   responseType : "arraybuffer",
-    // });
-    //  if (response.status == 200) {
-    //   imageHtml = iconv.decode(response.data, 'EUC-KR').toString();
-    //   const $ = cheerio.load(imageHtml);
-      
 
 crawler()
-// schedule.scheduleJob('48 04 * * *', ()=>{
-// (async() =>{
-//   await webhook.send({
-//     text:"안녕하세요? 날씨봇입니다.",
-//     attachments:[
-//       {
-//         fallback:"<https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=%EB%82%B4%EC%9D%BC+%EB%82%A0%EC%94%A8| Tomorrow's weather>",
-//         pretext:"<https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=%EB%82%B4%EC%9D%BC+%EB%82%A0%EC%94%A8| Tomorrow's weather>",
-//         color: "#FFFFFF",
-//         fields:[
-//           {
-//             title:"내일 입을 옷이 고민이신가요?",
-//             value:"매일밤 10시 다음날의 날씨를 알려드립니다!",
-//             short:false}
-//               ]
-//             }
-//           ]
-//         }, function(err,response){
-//           console.log(response);
-//         }
-//       );
-//     }
-// )()
-// });
-
-      // (async () => {
-      //     await webhook.send({
-      //       text: '날씨봇 ON!! 11시가 되면 날씨를 알려드리겠습니다.',
-      //     });
-      //   })();
-
 
   app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
--- node-schedual.js
+++ node-schedual.js
@@ -1,1 +1,60 @@
-const schedual = require('node-schedule');
+function onInstallation(bot, installer) {
+    if (installer) {
+        bot.startPrivateConversation({user: installer}, function (err, convo) {
+            if (err) {
+                console.log(err);
+            } else {
+                convo.say('I am a bot that has just joined your team');
+                convo.say('You must now /invite me to a channel so that I can be of use!');
+            }
+        });
+    }
+}
+
+var config = {};
+if (process.env.MONGOLAB_URI) {
+    var BotkitStorage = require('botkit-storage-mongo');
+    config = {
+        storage: BotkitStorage({mongoUri: process.env.MONGOLAB_URI}),
+    };
+} else {
+    config = {
+        json_file_store: ((process.env.TOKEN)?'./db_slack_bot_ci/':'./db_slack_bot_a/'), 
+    };
+}
+
+if (process.env.TOKEN || process.env.SLACK_TOKEN) {
+    var customIntegration = require('./lib/custom_integrations');
+    var token = (process.env.TOKEN) ? process.env.TOKEN : process.env.SLACK_TOKEN;
+    var controller = customIntegration.configure(token, config, onInstallation);
+} else if (process.env.CLIENT_ID && process.env.CLIENT_SECRET && process.env.PORT) {
+    var app = require('./lib/apps');
+    var controller = app.configure(process.env.PORT, process.env.CLIENT_ID, process.env.CLIENT_SECRET, config, onInstallation);
+} else {
+    console.log('Error: If this is a custom integration, please specify TOKEN in the environment. If this is an app, please specify CLIENTID, CLIENTSECRET, and PORT in the environment');
+    process.exit(1);
+}
+
+controller.on('rtm_open', function (bot) {
+    console.log('** The RTM api just connected!');
+});
+
+controller.on('rtm_close', function (bot) {
+    console.log('** The RTM api just closed');
+});
+
+controller.on('bot_channel_join', function (bot, message) {
+    bot.reply(message, "I'm here!")
+});
+
+controller.hears ( 
+    [ 'hello', 'hi', 'greetings'], 
+    [ 'direct_mention', 'mention', 'direct_message'], 
+    function (bot, message) { 
+        bot.reply (message, 'Hello!') ; 
+    } 
+);
+
+controller.hears ('됬','ambient',function(bot, message){
+    bot.reply(message, '됬이 아니라 됐입니다!');
+});
