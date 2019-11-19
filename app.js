cb8b3bf69eeea41376188637761a635751083289
cosmian <softkey95@gmail.com>
2019-11-20 02:02:02 +0900

    killed server

--- app.js
+++ app.js
@@ -60,6 +60,6 @@ const crawler = async() => {
 
 crawler()
 
-  app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-  });
+  // app.listen(3000, function () {
+  //   console.log('Example app listening on port 3000!');
+  // });
